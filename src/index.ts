f4cea6e67145f16f55a6c7e0537e00637f4920f9
pbutsik <p.butsik@gmail.com>
2024-06-09 10:01:33 +0300

    errors

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+// const numbers:number[] = [2, 3, 5];
+
+// // Стрелочная функция. Не запнётся ли на ней Internet Explorer?
+// const doubledNumbers = numbers.map(number => number * 2);
+
+// console.log(doubledNumbers); // 4, 6, 10
+
+
+import style from './index.module.scss';
+import './index.scss';
+
+console.log(style); //использование объекта style важно для попадания стилей в css-бандл
